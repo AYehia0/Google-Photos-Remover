6d174c54ab50970264cda5a2bd94a28307e35f5b
AYehia0 <ahmedyehia611@gmail.com>
2021-08-01 15:08:52 +0200

    moved the config vars to a file

---
+++ config.py
@@ -0,0 +1,17 @@
+APP_NAME = "photoslibrary"
+API_VERSION = "v1"
+CRED_FILE = "secrets.json"
+SCOPES = [
+    "https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata",
+    "https://www.googleapis.com/auth/photoslibrary.readonly"
+]
+LOG_FILE_NAME = "photo_logs.json"
+
+# for video extentions
+VID_EXT = ['mp4', 'flv', 'mkv', 'gif', 'avi', 'wmv', '3gp', 'mpv', 'amv', 'mpeg', 'm2v']
+
+# to bypass the google authentication
+# Add your path 
+PROFILE_PATH = "/home/none/.mozilla/firefox/iks1vvci.default-release"
+DEBUG = False
+
--- main.py
+++ main.py
@@ -34,22 +34,9 @@ from custom_google import Create_Service
 # important : for deleting and doing some web automation
 from scrape_main import *
 
-APP_NAME = "photoslibrary"
-API_VERSION = "v1"
-CRED_FILE = "secrets.json"
-SCOPES = [
-    "https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata",
-    "https://www.googleapis.com/auth/photoslibrary.readonly"
-]
-LOG_FILE_NAME = "photo_logs.json"
+# loading the configs
+from config import *
 
-# for video extentions
-VID_EXT = ['mp4', 'flv', 'mkv', 'gif', 'avi', 'wmv', '3gp', 'mpv', 'amv', 'mpeg', 'm2v']
-
-# to bypass the google authentication
-# Add your path 
-PROFILE_PATH = "/home/none/.mozilla/firefox/iks1vvci.default-release"
-DEBUG = False
 
 def save_image(url, destination_folder, file_name):
     """Save image to a file"""
